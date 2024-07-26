d692a13950a5b252a6a07f00b6f85111e82ad99b
wallaby150 <duddlf1025@gmail.com>
2024-07-26 15:51:33 +0900

    할일 완료 체크박스 추가

--- front-app-todo-openapi/src/todos/TodoItem.jsx
+++ front-app-todo-openapi/src/todos/TodoItem.jsx
@@ -3,36 +3,42 @@ import { useNavigate } from "react-router-dom";
 const TodoItem = ({ todoItem, callbacks }) => {
   const navigate = useNavigate();
 
-  let itemClassName = "list-group-item";
-  if (todoItem.done)
-    itemClassName += " list-group-item-success text-decoration-line-through";
-
   return (
-    <li className={itemClassName}>
-      <span
-        onClick={() => {
-          callbacks.toggleDone(todoItem.id);
-        }}
-        title={todoItem.desc}
-      >
-        {todoItem.todo}
-      </span>
-      <span
-        className="float-end badge bg-secondary pointer p-2 m-1"
-        onClick={() => {
-          navigate("/todos/edit/" + todoItem.id);
-        }}
-      >
-        편집
-      </span>
-      <span
-        className="float-end badge bg-secondary pointer p-2 m-1"
-        onClick={() => {
-          callbacks.deleteTodo(todoItem.id);
-        }}
-      >
-        삭제
-      </span>
+    <li
+      className={`list-group-item d-flex align-items-center justify-content-between ${
+        todoItem.done
+          ? "list-group-item-success text-decoration-line-through"
+          : ""
+      }`}
+    >
+      <div>
+        <input
+          type="checkbox"
+          className="me-2"
+          checked={todoItem.done}
+          onChange={() => callbacks.toggleDone(todoItem.id)}
+        ></input>
+
+        <span title={todoItem.desc}>{todoItem.todo}</span>
+      </div>
+      <div>
+        <span
+          className="float-end badge bg-secondary pointer p-2 m-1"
+          onClick={() => {
+            navigate("/todos/edit/" + todoItem.id);
+          }}
+        >
+          편집
+        </span>
+        <span
+          className="float-end badge bg-secondary pointer p-2 m-1"
+          onClick={() => {
+            callbacks.deleteTodo(todoItem.id);
+          }}
+        >
+          삭제
+        </span>
+      </div>
     </li>
   );
 };
